71c65e6df4d406970cb5ab4b37d5c9d54e0fa6ba
Serkan İnce <serkan_goldfire@hotmail.com>
2016-05-17 14:54:32 +0300

    firs check-in

---
+++ Mvc Sample Project/Sample01-Bundle/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Sample01_Bundle.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/Css").Include(
+                "~/assets/bootstrap-{version}.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/JS").Include(
+                "~/assets/jquery-{version}.js",
+                "~/assets/bootstrap-{version}.js"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ Mvc Sample Project/Sample01-Bundle/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using Sample01_Bundle.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Sample01_Bundle
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
